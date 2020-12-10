2c57522b5ae4ad1b693d3cef70b73310c47dca84
yan00102 <yan00102@algonquinlive.com>
2020-12-10 16:33:20 -0500

    Displaying name, phone, distance, price, rating and image in the ThirdScreen

--- screens/ThirdScreen.js
+++ screens/ThirdScreen.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Image } from "react-native";
 
-export default class ThirdScreen extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>This is ThirdScreen.</Text>
-      </View>
-    );
-  }
+export default function ThirdScreen({ route }) {
+  const { selectedItem } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {/* <Text>Third Screen</Text> */}
+      <Text>{selectedItem.name}</Text>
+      <Text>
+        {selectedItem.phone.slice(0, 4) +
+          "-" +
+          selectedItem.phone.slice(4, 7) +
+          "-" +
+          selectedItem.phone.slice(7, 11)}
+      </Text>
+      <Text>{(selectedItem.distance / 1000).toFixed(2) + "KM"}</Text>
+      <Text>{selectedItem.price}</Text>
+      <Text>{selectedItem.rating}</Text>
+      <Image
+        source={{ uri: selectedItem.image_url }}
+        style={{ width: 200, height: 200 }}
+      />
+    </View>
+  );
 }
