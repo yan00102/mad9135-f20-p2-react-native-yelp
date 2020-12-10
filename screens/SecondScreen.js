e5adf6df0fa720927d942c8f818a48315cec5772
yan00102 <yan00102@algonquinlive.com>
2020-12-10 16:29:09 -0500

    Displaying nearby restaurants in SecondScreen and Click on each of them to navigate to ThirdScreen

--- screens/SecondScreen.js
+++ screens/SecondScreen.js
@@ -8,16 +8,61 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-export default class SecondScreen extends Component {
-  render() {
+export default function SecondScreen({ route, navigation }) {
+  const { data } = route.params;
+  // console.log(data);
+
+  const Item = ({ name, distance, style, onPress }) => {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>This is SecondScreen.</Text>
-        <Button
-          title="Nearby Restaurants"
-          onPress={() => this.props.navigation.navigate("ThirdScreen About Us")}
-        />
-      </View>
+      <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+        <Text style={styles.title}>Restaurants: {name}</Text>
+        <Text style={styles.title}>
+          Distance: {(distance / 1000).toFixed(2)}
+        </Text>
+      </TouchableOpacity>
     );
-  }
+  };
+
+  const findSelectedRes = (item) => {
+    navigation.navigate("ThirdScreen About Us", {
+      selectedItem: item,
+    });
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <Item
+        name={item.name}
+        distance={item.distance}
+        item={item}
+        onPress={() => findSelectedRes(item)}
+      />
+    );
+  };
+
+  const styles = StyleSheet.create({
+    item: {
+      backgroundColor: "#f9c2ff",
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
+
+  return (
+    <View>
+      <FlatList
+        data={data.businesses.sort((a, b) => a.distance - b.distance)}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+      <Button
+        title="Nearby Restaurants"
+        onPress={() => navigation.navigate("ThirdScreen About Us")}
+      />
+    </View>
+  );
 }
