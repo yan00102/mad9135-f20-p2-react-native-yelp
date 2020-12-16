e1a8a4640a845a48f75260a26a4ee41c1b3c01e4
yan00102 <yan00102@algonquinlive.com>
2020-12-16 02:02:59 -0500

    use library react-native-loading-spinner-overlay to display a spinner when fetching

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, Button } from "react-native";
 import * as Location from "expo-location"; // expo-location allows reading geolocation information from the device.
 import * as Permissions from "expo-permissions"; //expo-permissions will ask for the user's permission first
+import Spinner from "react-native-loading-spinner-overlay"; //loading overlay
 
 export default class HomeScreen extends Component {
   state = {
+    loading: false,
     longitude: null,
     latitude: null,
     API_KEY:
       "GePd89bGyGTrP3_4k6JJ4sv0W1pn99sL38JxuNUEb3H4uOyI3m5P8ZkhqK7CqxAPRwfhfDB-_pAgVmx2bdW708H74m43vDbM_cmhhPJXxof7hWVoFW4VJ22QHJwIX3Yx",
   };
 
+  startLoading = () => {
+    this.setState(
+      { loading: true },
+      this.getLocation
+      // setTimeout(() => {
+      //   this.setState({ loading: false });
+      // }, 5000)
+    );
+  };
+
   getLocation = async () => {
+    this.startLoading;
     let { status } = await Location.requestPermissionsAsync(
       Permissions.LOCATION
     );
@@ -23,6 +36,7 @@ export default class HomeScreen extends Component {
       let currLocation = await Location.getCurrentPositionAsync({
         accuracy: Location.Accuracy.High,
       });
+
       this.setState(
         {
           longitude: currLocation.coords.longitude,
@@ -51,6 +65,7 @@ export default class HomeScreen extends Component {
     fetch(req)
       .then((res) => res.json())
       .then((data) => {
+        this.setState({ loading: false });
         this.props.navigation.navigate("SecondScreen Nearby Restaurants", {
           data: data,
         });
@@ -61,16 +76,43 @@ export default class HomeScreen extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Search Restaurants"
-          onPress={this.getLocation}
-          //   onPress={() =>
-          //     this.props.navigation.navigate("SecondScreen Nearby Restaurants")
-          //   }
-        />
-      </View>
+      <SafeAreaView
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
+        <View style={styles.container}>
+          <Text>Home Screen</Text>
+          <Spinner
+            //visibility of Overlay Loading Spinner
+            visible={this.state.loading}
+            //Text with the Spinner
+            textContent={"Loading..."}
+            //Text style of the Spinner Text
+            textStyle={styles.spinnerTextStyle}
+          />
+          <Button
+            title="Search Restaurants"
+            onPress={this.startLoading}
+            //   onPress={() =>
+            //     this.props.navigation.navigate("SecondScreen Nearby Restaurants")
+            //   }
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    paddingTop: 30,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  spinnerTextStyle: {
+    color: "#FFF",
+  },
+});
